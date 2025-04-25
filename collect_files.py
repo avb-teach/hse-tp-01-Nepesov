c81796023bb338488b0b333ef98a3144c46e552a
B <nepesov82@mail.ru>
2025-04-26 00:17:52 +0300

    sixth  commit to check all exercise with chmod and comments

--- collect_files.py
+++ collect_files.py
@@ -2,17 +2,20 @@
 
 #делаем шебанг для использования python
 
-import sys
-import os
-import shutil 
+import sys # импортируем sys для получения аргументов
+import os #os для получения путей и проходок по полученным аргументам walk, makedirs and path 
+import shutil #shutil нужен для копирования src-ишников 
 
-input_directory = None
+input_directory = None #оставляем переменные в состоянии None тк нам изначально неизвестна последовательность получения аргументов
 output_directory = None
 max_depth = None 
 
+# получаем аргументы 
 args = sys.argv[1:]
+#счетчик аргов
 i = 0 
 
+#попытка получения аргумента --max_depth с выводом ошибок, определение входных и выходеных директорий 
 while i < len(args):
     if args[i] == "--max_depth":
         try:
@@ -30,41 +33,43 @@ while i < len(args):
         else:
             output_directory = args[i]
         i +=1
-    
 
+# создает директории в выходной директории, и если папка сущ -> пропуск из-за второго параметра
 os.makedirs(output_directory,exist_ok = True)
-
+#создание учетного словаря для сохранения имен файлов 
 accounting = {}
 
+#проход по аргументам подданым изначально current_dir - нынешняя директория, other_directories - другие директории , files -файлы
 for current_dir, other_directories, files in os.walk(input_directory):
-    for name in files:
-        files_src = os.path.join(current_dir,name)
-        file_name , extension = os.path.splitext(name)
-
+    for name in files: # проход по файлам, для просмотра имен
+        files_src = os.path.join(current_dir,name) # сборка пути к файлу "./../../name.ext"
+        file_name , extension = os.path.splitext(name) # разделение на именную часть и расширение файла
+# создание счетчика для уникальности имен файлов
         if name not in accounting:
             accounting[name] = 0
-         
+        
         count = accounting[name]
 
         if count == 0:
             cur_name = name
         else:
-            cur_name = f"{file_name}_{count}{extension}" if extension else f"{name}_{count}"
+            cur_name = f"{file_name}_{count}{extension}" if extension else f"{name}_{count}" # создаем полное имя файла для неповторений, 
+            #с условием наличия расширения 
         
         
 
-        copied_file_path = os.path.join(output_directory, cur_name)
-        shutil.copy(files_src , copied_file_path)
+        copied_file_path = os.path.join(output_directory, cur_name) # создает путь для копии файла output_directory- куда копируем , cur_name - сгенерированное имя 
+        shutil.copy(files_src , copied_file_path) # копирует файл из files_src в copied_file_path
 
-        accounting[name] +=1
+        accounting[name] +=1 # плюс 1 к счетчику 
 
 
 
-    rel_path = os.path.relpath(current_dir,input_directory)
-    depth = 0 if rel_path == "." else rel_path.count(os.sep) + 1
+    rel_path = os.path.relpath(current_dir,input_directory) # переменная для определения глубины  внутри  input_directory
+    depth = 0 if rel_path == "." else rel_path.count(os.sep) + 1 # глубина 0, если текущ папка = входной, иначе количество /(os.sep) будет увеличено 
 
-    if max_depth != None and depth >= max_depth:
-        del other_directories[:]
+    if max_depth is not None and depth >= max_depth: #  проверка не превышена ли макс глубина
+        del other_directories[:] #если превышена то очистить список этих папок иначе зачем их проверять ? 
      
 
 #https://javarush.com/quests/lectures/ru.javarush.python.core.lecture.level12.lecture06
